83a8b46fcaefcfaa4f83064d6b59837d7a73243f
fuller2014 <esoto.torre@gmail.com>
2014-02-08 20:02:34 -0500

    Modulo Esoto

--- czf2/config/application.config.php
+++ czf2/config/application.config.php
@@ -6,6 +6,7 @@ return array(
         'Admin',
         'AlumnoEanaya',
         'AlumnoCsernaque',
+        'AlumnoEsoto',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
