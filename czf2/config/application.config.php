aff96f2783ed7b36fc1bcba0fd4d27e5ae8368c0
Ernesto Anaya <ernestex@gmail.com>
2014-02-08 19:32:37 -0500

    Modulo EAnaya

--- czf2/config/application.config.php
+++ czf2/config/application.config.php
@@ -4,6 +4,7 @@ return array(
     'modules' => array(
         'Application',
         'Admin',
+        'AlumnoEanaya',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
