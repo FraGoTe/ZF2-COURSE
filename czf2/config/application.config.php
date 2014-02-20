308e38548d3cdef838dc37444feeb31ca888bb41
Jose Pomalaza <jpomalaza@gibarcena.com.pe>
2014-02-20 17:11:35 -0500

    modificacion para trabajar con tema-db

--- czf2/config/application.config.php
+++ czf2/config/application.config.php
@@ -8,9 +8,9 @@ return array(
         'AlumnoEsoto',
         'AlumnoEanaya',
         'AlumnoCsernaque',        
-        'AlumnoOgines',
-        'TemaDb',
+        'AlumnoOgines',        
         'AlbumJpomalaza',
+        'TemaDb',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
